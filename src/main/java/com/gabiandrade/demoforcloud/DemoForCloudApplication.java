03c1c87da7ac7be9bb9dc894ea118bbba03a1c32
gabiandrade <gabrielav.sudo@gmail.com>
2023-04-14 20:38:37 -0300

    Initial Commit

---
+++ src/main/java/com/gabiandrade/demoforcloud/DemoForCloudApplication.java
@@ -0,0 +1,13 @@
+package com.gabiandrade.demoforcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabiandrade/demoforcloud/DemoForCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabiandrade.demoforcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
